2be2d0514aa219f1a17f7f961a2030a8192dcd90
Keto K. <kirua@duck.com>
2025-08-16 05:52:15 -0600

    Update index.tsx

--- src/apps/internet-explorer/index.tsx
+++ src/apps/internet-explorer/index.tsx
@@ -44,10 +44,10 @@ export const appMetadata = {
   version: "1.02",
   name: "Internet Explorer",
   creator: {
-    name: "Ryo Lu",
-    url: "https://ryo.lu",
+    name: "Keto Khamkhounmavong",
+    url: "https://keto.kh",
   },
-  github: "https://github.com/ryokun6/ryos",
+  github: "https://github.com/codekirua/KetoOS",
   icon: "/icons/default/ie.png",
 };
 
