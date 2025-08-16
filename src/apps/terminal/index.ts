7ec68433f834cbecb3dedc310ad1f127933392a0
Keto K. <kirua@duck.com>
2025-08-16 05:27:30 -0600

    Update index.ts

--- src/apps/terminal/index.ts
+++ src/apps/terminal/index.ts
@@ -52,10 +52,10 @@ export const TerminalApp: BaseApp = {
     name: "Terminal",
     version: "1.0",
     creator: {
-      name: "Ryo Lu",
-      url: "https://ryo.lu",
+      name: "Keto Khamkhounmavong",
+      url: "https://keto.kh",
     },
-    github: "https://github.com/ryokun6/ryos",
+    github: "https://github.com/codekirua/KetoOS",
     icon: "/icons/default/terminal.png",
   },
 };
